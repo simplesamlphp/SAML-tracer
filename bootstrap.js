4e462bbf4cc2d4ce77c2fbe59b5393d08f080158
Jan Köhler <khlr.jn@gmail.com>
2017-10-22 16:27:03 +0200

    Added a manifest.json and a minimal TraceWindow-HTML-document.

--- bootstrap.js
+++ bootstrap.js
@@ -1,146 +1,167 @@
-// This bootstrap.js file is based upon the template available here:
-// https://developer.mozilla.org/en-US/Add-ons/Firefox_for_Android/Initialization_and_Cleanup#template_code
-// The onOpenWindow event handler was slightly modified to be compatible with standard Firefox.
+function onCreated(windowInfo) {
+  console.log("Created window: ${windowInfo.id}");
+}
 
-var Cc = Components.classes;
-var Ci = Components.interfaces;
-var Cu = Components.utils;
+function onError(error) {
+  console.log("Error: ${error}");
+}
 
-Cu.import("resource:///modules/CustomizableUI.jsm");
-Cu.import('resource://gre/modules/Services.jsm');
-
-var css_uri = Services.io.newURI("chrome://samltrace/skin/button.css", null, null);
-var strings = Services.strings.createBundle('chrome://samltrace/locale/samltrace.properties');
-
-var tracerWindow = null;
-function showTracerWindow() {
-  if (tracerWindow != null) {
-    // Window already opened -- just give it focus.
-    tracerWindow.focus();
-    return;
-  }
-
-  tracerWindow = Services.ww.openWindow(null, "chrome://samltrace/content/TraceWindow.xul", "global:samltrace", "chrome,centerscreen,resizable", null);
-  tracerWindow.addEventListener('close', function() {
-    tracerWindow = null;
+browser.browserAction.onClicked.addListener((tab) => {
+  var url = browser.extension.getURL("/chrome/samltrace/content/TraceWindow.html");
+  var creating = browser.windows.create({
+    url: url,
+    type: "panel",
+    height: 400,
+    width: 600
   });
-}
+  creating.then(onCreated, onError);
+});
 
 
-function loadIntoWindow(window) {
-  if (!window)
-    return;
 
-  // Add our style sheet
-  window.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowUtils).loadSheet(css_uri, 1);
+// // This bootstrap.js file is based upon the template available here:
+// // https://developer.mozilla.org/en-US/Add-ons/Firefox_for_Android/Initialization_and_Cleanup#template_code
+// // The onOpenWindow event handler was slightly modified to be compatible with standard Firefox.
 
-  // Add an entry to the tools menu.
-  let menuToolsPopup = window.document.getElementById('menu_ToolsPopup');
-  if (menuToolsPopup) {
-    let mi = window.document.createElementNS('http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul', 'menuitem');
-    mi.setAttribute('label', strings.GetStringFromName('samltrace.open.label'));
-    mi.setAttribute('id', 'samltrace-menu-open');
-    mi.addEventListener('command', showTracerWindow);
-    menuToolsPopup.appendChild(mi);
-  }
+// var Cc = Components.classes;
+// var Ci = Components.interfaces;
+// var Cu = Components.utils;
 
-  // Add it to the web developer menu.
-  let menuWebDevPopup = window.document.getElementById('appmenu_webDeveloper_popup');
-  if (menuWebDevPopup) {
-    let mi = window.document.createElementNS('http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul', 'menuitem');
-    mi.setAttribute('label', strings.GetStringFromName('samltrace.open.label'));
-    mi.setAttribute('id', 'samltrace-appmenu-open');
-    mi.addEventListener('command', showTracerWindow);
-    let errConsole = window.document.getElementById("appmenu_errorConsole");
-    if (errConsole && errConsole.parentNode == menuWebDevPopup) {
-      /* Insert it before the error console, if it is in the menu. */
-      menuWebDevPopup.insertBefore(mi, errConsole);
-    } else {
-      /* No error console -- just append it to the end. */
-      menuWebDevPopup.appendChild(mi);
-    }
-  }
-}
+// Cu.import("resource:///modules/CustomizableUI.jsm");
+// Cu.import('resource://gre/modules/Services.jsm');
 
-function unloadFromWindow(window) {
-  if (!window)
-    return;
+// var css_uri = Services.io.newURI("chrome://samltrace/skin/button.css", null, null);
+// var strings = Services.strings.createBundle('chrome://samltrace/locale/samltrace.properties');
 
-  // Clean up menu entries.
-  let toolsOpen = window.document.getElementById('samltrace-menu-open');
-  if (toolsOpen) {
-    toolsOpen.parentNode.removeChild(toolsOpen);
-  }
-  let appMenuOpen = window.document.getElementById('samltrace-appmenu-open');
-  if (appMenuOpen) {
-    appMenuOpen.parentNode.removeChild(appMenuOpen);
-  }
+// var tracerWindow = null;
+// function showTracerWindow() {
+//   if (tracerWindow != null) {
+//     // Window already opened -- just give it focus.
+//     tracerWindow.focus();
+//     return;
+//   }
 
-  // Remove our style sheet
-  window.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowUtils).removeSheet(css_uri, 1);
-}
+//   tracerWindow = Services.ww.openWindow(null, "chrome://samltrace/content/TraceWindow.xul", "global:samltrace", "chrome,centerscreen,resizable", null);
+//   tracerWindow.addEventListener('close', function() {
+//     tracerWindow = null;
+//   });
+// }
 
-var windowListener = {
-  onOpenWindow: function(aWindow) {
-    // Wait for the window to finish loading
-    let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindow);
-    domWindow.addEventListener("load", function onLoad() {
-      domWindow.removeEventListener("load", onLoad, false);
-      loadIntoWindow(domWindow);
-    }, false);
-  },
 
-  onCloseWindow: function(aWindow) {},
-  onWindowTitleChange: function(aWindow, aTitle) {}
-};
+// function loadIntoWindow(window) {
+//   if (!window)
+//     return;
 
-function startup(aData, aReason) {
-  // Load into any existing windows
-  let windows = Services.wm.getEnumerator("navigator:browser");
-  while (windows.hasMoreElements()) {
-    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
-    loadIntoWindow(domWindow);
-  }
+//   // Add our style sheet
+//   window.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowUtils).loadSheet(css_uri, 1);
 
-  // Load into any new windows
-  Services.wm.addListener(windowListener);
+//   // Add an entry to the tools menu.
+//   let menuToolsPopup = window.document.getElementById('menu_ToolsPopup');
+//   if (menuToolsPopup) {
+//     let mi = window.document.createElementNS('http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul', 'menuitem');
+//     mi.setAttribute('label', strings.GetStringFromName('samltrace.open.label'));
+//     mi.setAttribute('id', 'samltrace-menu-open');
+//     mi.addEventListener('command', showTracerWindow);
+//     menuToolsPopup.appendChild(mi);
+//   }
 
-  // Add button to panel.
-  CustomizableUI.createWidget({
-    id: "samltrace-showtracer",
-    defaultArea: CustomizableUI.AREA_PANEL,
-    removable: true,
-    label: strings.GetStringFromName('samltrace.open.label'),
-    onCommand: showTracerWindow
-  });
+//   // Add it to the web developer menu.
+//   let menuWebDevPopup = window.document.getElementById('appmenu_webDeveloper_popup');
+//   if (menuWebDevPopup) {
+//     let mi = window.document.createElementNS('http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul', 'menuitem');
+//     mi.setAttribute('label', strings.GetStringFromName('samltrace.open.label'));
+//     mi.setAttribute('id', 'samltrace-appmenu-open');
+//     mi.addEventListener('command', showTracerWindow);
+//     let errConsole = window.document.getElementById("appmenu_errorConsole");
+//     if (errConsole && errConsole.parentNode == menuWebDevPopup) {
+//       /* Insert it before the error console, if it is in the menu. */
+//       menuWebDevPopup.insertBefore(mi, errConsole);
+//     } else {
+//       /* No error console -- just append it to the end. */
+//       menuWebDevPopup.appendChild(mi);
+//     }
+//   }
+// }
 
-}
+// function unloadFromWindow(window) {
+//   if (!window)
+//     return;
 
-function shutdown(aData, aReason) {
-  // When the application is shutting down we normally don't have to clean
-  // up any UI changes made
-  if (aReason == APP_SHUTDOWN) {
-    return;
-  }
+//   // Clean up menu entries.
+//   let toolsOpen = window.document.getElementById('samltrace-menu-open');
+//   if (toolsOpen) {
+//     toolsOpen.parentNode.removeChild(toolsOpen);
+//   }
+//   let appMenuOpen = window.document.getElementById('samltrace-appmenu-open');
+//   if (appMenuOpen) {
+//     appMenuOpen.parentNode.removeChild(appMenuOpen);
+//   }
 
-  // Close the SAML tracer window if it is open.
-  if (tracerWindow != null) {
-    tracerWindow.close();
-    tracerWindow = null;
-  }
+//   // Remove our style sheet
+//   window.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowUtils).removeSheet(css_uri, 1);
+// }
 
-  // Stop listening for new windows
-  Services.wm.removeListener(windowListener);
+// var windowListener = {
+//   onOpenWindow: function(aWindow) {
+//     // Wait for the window to finish loading
+//     let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindow);
+//     domWindow.addEventListener("load", function onLoad() {
+//       domWindow.removeEventListener("load", onLoad, false);
+//       loadIntoWindow(domWindow);
+//     }, false);
+//   },
 
-  // Unload from any existing windows
-  let windows = Services.wm.getEnumerator("navigator:browser");
-  while (windows.hasMoreElements()) {
-    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
-    unloadFromWindow(domWindow);
-  }
+//   onCloseWindow: function(aWindow) {},
+//   onWindowTitleChange: function(aWindow, aTitle) {}
+// };
 
-  CustomizableUI.destroyWidget("samltrace-showtracer");
-}
+// function startup(aData, aReason) {
+//   // Load into any existing windows
+//   let windows = Services.wm.getEnumerator("navigator:browser");
+//   while (windows.hasMoreElements()) {
+//     let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+//     loadIntoWindow(domWindow);
+//   }
 
-function install(aData, aReason) {}
-function uninstall(aData, aReason) {}
+//   // Load into any new windows
+//   Services.wm.addListener(windowListener);
+
+//   // Add button to panel.
+//   CustomizableUI.createWidget({
+//     id: "samltrace-showtracer",
+//     defaultArea: CustomizableUI.AREA_PANEL,
+//     removable: true,
+//     label: strings.GetStringFromName('samltrace.open.label'),
+//     onCommand: showTracerWindow
+//   });
+
+// }
+
+// function shutdown(aData, aReason) {
+//   // When the application is shutting down we normally don't have to clean
+//   // up any UI changes made
+//   if (aReason == APP_SHUTDOWN) {
+//     return;
+//   }
+
+//   // Close the SAML tracer window if it is open.
+//   if (tracerWindow != null) {
+//     tracerWindow.close();
+//     tracerWindow = null;
+//   }
+
+//   // Stop listening for new windows
+//   Services.wm.removeListener(windowListener);
+
+//   // Unload from any existing windows
+//   let windows = Services.wm.getEnumerator("navigator:browser");
+//   while (windows.hasMoreElements()) {
+//     let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+//     unloadFromWindow(domWindow);
+//   }
+
+//   CustomizableUI.destroyWidget("samltrace-showtracer");
+// }
+
+// function install(aData, aReason) {}
+// function uninstall(aData, aReason) {}
