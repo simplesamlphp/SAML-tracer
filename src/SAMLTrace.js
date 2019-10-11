6a6d2dc9e90a0ba7e242455389381b8ab7ed42cb
Jan Köhler <khlr.jn@gmail.com>
2019-10-11 17:47:33 +0200

    Added a new button which allows to stop or resume the colorization of traced requests.

--- src/SAMLTrace.js
+++ src/SAMLTrace.js
@@ -549,6 +549,7 @@ SAMLTrace.TraceWindow = function() {
   this.pauseTracing = false;
   this.autoScroll = true;
   this.filterResources = true;
+  this.colorizeRequests = true;
 };
 
 SAMLTrace.TraceWindow.prototype = {
@@ -641,6 +642,10 @@ SAMLTrace.TraceWindow.prototype = {
   'setFilterResources' : function(filterResources) {
     this.filterResources = filterResources;
     this.updateStatusBar();
+  },
+
+  'setColorizeRequests' : function(colorizeRequests) {
+    this.colorizeRequests = colorizeRequests;
   },
 
   'updateStatusBar' : function() {
@@ -757,6 +762,10 @@ SAMLTrace.TraceWindow.prototype = {
           if (!tracer.filterResources) {
             requestDiv.classList.add("displayAnyway");
           }
+        }
+
+        if (!tracer.colorizeRequests) {
+          requestDiv.classList.add("monochrome");
         }
         
         entry.isVisible = isVisible;
