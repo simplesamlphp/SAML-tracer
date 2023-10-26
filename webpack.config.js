d0f01da6c730297526ee839beec3ccbcff71a076
Tim van Dijen <tvdijen@gmail.com>
2023-10-26 21:52:41 +0200

    Build highlight.js with the necessary languages

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const buildDir = __dirname + '/lib';
+
+const localConfig = {
+    js_filename: '[name].js'
+};
+
+module.exports = environment => {
+    return {
+        entry: {
+            highlight: './src/resources/js/highlight'
+        },
+        output: {
+            path: path.resolve(buildDir),
+            filename: localConfig['js_filename']
+        },
+        mode: 'production',
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ]
+        },
+        optimization: {
+            minimizer: [new TerserPlugin({
+                extractComments: false,
+            })],
+        },
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname + '/node_modules/pako/dist/pako_inflate.js'),
+                        to: '[name][ext]'
+                    }
+                ]
+            })
+        ]
+    }
+};
