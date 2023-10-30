50840a06df26f422c1e4c9731f1090580a28a95e
Tim van Dijen <tvdijen@gmail.com>
2023-10-30 10:42:17 +0100

    Restore @babel/preset-env

--- src/resources/js/highlight.js
+++ src/resources/js/highlight.js
@@ -1,6 +1,5 @@
 'use strict';
 
-import * as poly from "@babel/polyfill/dist/polyfill";
 import hljs from  "highlight.js/lib/core";
 import http from "highlight.js/lib/languages/http";
 import xml from "highlight.js/lib/languages/xml";
