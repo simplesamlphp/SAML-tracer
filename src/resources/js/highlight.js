d78c1f15f9889d0cb05646c98cf4508c8055bc1b
Tim van Dijen <tvdijen@gmail.com>
2023-10-30 10:49:59 +0100

    Include properties-language

--- src/resources/js/highlight.js
+++ src/resources/js/highlight.js
@@ -2,9 +2,11 @@
 
 import hljs from  "highlight.js/lib/core";
 import http from "highlight.js/lib/languages/http";
+import properties from "highlight.js/lib/languages/properties";
 import xml from "highlight.js/lib/languages/xml";
 
 ready(function () {
     hljs.registerLanguage('http', http);
+    hljs.registerLanguage('properties', properties);
     hljs.registerLanguage('xml', xml);
 });
