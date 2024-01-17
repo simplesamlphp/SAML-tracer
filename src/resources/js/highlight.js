6a10f99d277ddbcd865d84e90dd296b007c6832e
Tim van Dijen <tvdijen@gmail.com>
2024-01-17 17:46:54 +0100

    Bump upload-artifact action

--- src/resources/js/highlight.js
+++ src/resources/js/highlight.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var hljs;
-
 import {highlight as hljs} from  "highlight.js/lib/core";
 import http from "highlight.js/lib/languages/http";
 import properties from "highlight.js/lib/languages/properties";
