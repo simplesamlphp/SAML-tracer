04b5585033664a75ec7fbc2cae23117bf98cb890
Tim van Dijen <tvdijen@gmail.com>
2024-02-16 21:39:19 +0100

    Autobuild release when a tag is set (#87)
    * Automate build-process

---
+++ bin/build.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e
+
+CWD="$(pwd)"
+
+HLJS_VERSION=11.9.0
+PAKO_VERSION=2.1.0
+
+if [ -n "$1" ]; then
+    RELEASE="tags/$1"
+    TARGET="/tmp/$1.zip"
+else
+    RELEASE="HEAD"
+    TARGET="/tmp/samltracer.zip"
+fi
+
+if ! git rev-parse --verify "$RELEASE" >/dev/null 2>&1; then
+    echo "Invalid revision: $RELEASE" >&2
+    exit 1
+fi
+
+# Build Highlight.js
+HLJS=$(mktemp -d)
+cd "$HLJS"
+git clone -b "$HLJS_VERSION" https://github.com/highlightjs/highlight.js.git
+cd highlight.js
+
+npm install --package-lock-only
+npm clean-install
+npm run build
+node tools/build.js xml properties http
+
+# Build Pako
+PAKO=$(mktemp -d)
+cd "$PAKO"
+git clone -b "$PAKO_VERSION" https://github.com/nodeca/pako.git
+cd pako
+
+npm install --package-lock-only
+npm clean-install
+npm run build
+
+DEST=$(mktemp -d)
+cd $CWD
+cp -R $CWD/. $DEST
+cd $DEST
+
+# Copy dependencies
+cp "$HLJS/highlight.js/build/highlight.min.js" "$DEST/lib/"
+cp "$PAKO/pako/dist/pako_inflate.min.js" "$DEST/lib/"
+
+# Clean release
+grep export-ignore .gitattributes | cut -d ' ' -f 1 | while IFS= read -r line
+do
+  rm -rf "$line"
+done
+
+git archive --format tar "$RELEASE" | (cd "$DEST"; tar xv)
+
+cd "$DEST"
+
+zip -r "$TARGET" *
+
+cd /
+rm -rf "$DEST"
+rm -rf "$HLJS"
+rm -rf "$PAKO"
